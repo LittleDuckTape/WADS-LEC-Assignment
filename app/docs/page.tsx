af089e850ee92664da6680173db045af84c4d7b4
LittleDuckTape <ireneangelina99@gmail.com>
2026-03-04 13:59:42 +0700

    assignment 4 update

---
+++ app/docs/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import SwaggerUI from "swagger-ui-react";
+import { openapiSpec } from "@/lib/openapi";
+
+export default function DocsPage() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>API Documentation</h1>
+      <SwaggerUI spec={openapiSpec} />
+    </main>
+  );
+}
